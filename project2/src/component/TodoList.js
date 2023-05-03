fa18316a45b97d0987536ab88ee1cf354d14e2b2
beark93 <beark93@gmail.com>
2023-05-03 20:24:46 +0900

    project2 - todo 생성, 검색

--- project2/src/component/TodoList.js
+++ project2/src/component/TodoList.js
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
-const TodoList = () => {
+const TodoList = ({ todo }) => {
+  const [search, setSearch] = useState('');
+
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const getSearchResult = () => {
+    return search === ""
+      ? todo 
+      : todo.filter(item => item.content.toLowerCase().includes(search.toLowerCase()));
+  };
+
   return (
     <div className="TodoList">
       <h4>Todo List 🌱</h4>
-      <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChange}
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+      />
       <div className="list_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {getSearchResult().map(item => ( 
+          <TodoItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
