84d8056351d2e9b7b1d66374a2c4caeb9e4c6d4e
beark93 <beark93@gmail.com>
2023-05-12 21:02:05 +0900

    project3 - Editor 컴포넌트 생성

--- project3/src/pages/Home.js
+++ project3/src/pages/Home.js
@@ -1,32 +1,4 @@
-import Button from "../component/Button";
-import Header from "../component/Header";
-
 const Home = () => {
-  return (
-    <div>
-      <Header 
-        title={"Home"}
-        leftChild={
-          <Button 
-            type="positive"
-            text={"긍정 버튼"}
-            onClick={() => {
-              alert("positive button");
-            }}
-          />
-        }
-        rightChild={
-          <Button 
-            type="negative"
-            text={"부정 버튼"}
-            onClick={() => {
-              alert("negative button");
-            }}
-          />
-        }
-      />
-      
-    </div>
-  );
+  return <div>Home 페이지입니다</div>
 };
 export default Home;
