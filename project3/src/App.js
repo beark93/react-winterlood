a0f54cb73847cdb61873871f95f141142931533b
beark93 <beark93@gmail.com>
2023-06-01 22:17:00 +0900

    chapter10 - 완

--- project3/src/App.js
+++ project3/src/App.js
@@ -6,43 +6,27 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 
-const mokData = [
-  {
-    id: "mock1",
-    date: new Date().getTime() - 1,
-    content: "mock1",
-    emotionId: 1,
-  },
-  {
-    id: "mock2",
-    date: new Date().getTime() - 2,
-    content: "mock2",
-    emotionId: 2,
-  },
-  {
-    id: "mock3",
-    date: new Date().getTime() - 3,
-    content: "mock3",
-    emotionId: 3,
-  },
-]
-
 const reducer = (state, action) => {
   switch (action.type) {
-    case "CREATE": {
-      state = [action.data, ...state];
-      return state;
-    }
-    case "UPDATE": {
-      return state.map(it => 
-        String(it.id) === String(action.data.id) ? { ...action.data } : it
-      );
-    }
-    case "DELETE": {
-      return state.filter(it => String(it.id) !== String(action.targetId));
-    }
     case "INIT": {
       return action.data;
+    }
+    case "CREATE": {
+      const newState = [action.data, ...state];
+      localStorage.setItem("diary", JSON.stringify(newState));
+      return newState;
+    }
+    case "UPDATE": {
+      const newState = state.map(it => 
+        String(it.id) === String(action.data.id) ? { ...action.data } : it
+      );
+      localStorage.setItem("diary", JSON.stringify(newState));
+      return newState;
+    }
+    case "DELETE": {
+      const newState = state.filter(it => String(it.id) !== String(action.targetId));
+      localStorage.setItem("diary", JSON.stringify(newState));
+      return newState;
     }
     default: {
       return state;
@@ -59,10 +43,19 @@ function App() {
   const idRef = useRef(0);
 
   useEffect(() => {
-    dispatch({
-      type: "INIT",
-      data: mokData,
-    });
+    const rawData = localStorage.getItem("diary");
+    if (!rawData) {
+      setIsDataLoaded(true);
+      return;
+    }
+    const localData = JSON.parse(rawData);
+    if (localData.length === 0) {
+      setIsDataLoaded(true);
+      return;
+    }
+    localData.sort((a, b) => Number(b.id) - Number(a.id));
+    idRef.current = localData[0].id + 1;
+    dispatch({ type: "INIT", data: localData });
     setIsDataLoaded(true);
   }, []);
 
